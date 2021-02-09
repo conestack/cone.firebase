17b5fdc3aa893d67c1b7b74ef45685aa54f5b773
Philipp Auersperg-Castell <philk@bluedynamics.com>
2021-02-09 11:12:27 +0100

    exception handling for send_message_to_user, because send_multicast does not work

--- src/cone/firebase/messaging.py
+++ src/cone/firebase/messaging.py
@@ -4,6 +4,7 @@ Firebase Messaging with cone
 docs see:
 https://firebase.google.com/docs/cloud-messaging/send-message
 """
+from typing import List, Dict, Any
 import logging
 import firebase_admin
 from firebase_admin import messaging
@@ -13,21 +14,24 @@ from firebase_admin.messaging import UnregisteredError
 
 logger = logging.getLogger('cone.firebase')
 
-def send_message(data, token):
+def send_message(data: Dict[str, Any], token: str) -> str:
+    """
+
+    """
     messaging = firebase_admin.messaging
     message = messaging.Message(data=data, token=token)
     response = messaging.send(message)
     return response
 
 
-def send_messages(data, tokens):
+def send_messages(data: Dict[str, Any], tokens: List[str]):
     messaging = firebase_admin.messaging
     message = messaging.MulticastMessage(data=data, tokens=tokens)
     response = messaging.send_multicast(message)
     return response
 
 
-def send_message_to_user(login, data):
+def send_message_to_user(login: str, data: Dict[str, Any]) -> List[str]:
     tokens = get_device_tokens_for_user(login)
     results = []
     # return send_messages(data, tokens)
