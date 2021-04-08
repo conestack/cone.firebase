fbf53a06ff3d98b5bbe3504805275324a3c50dc0
Philipp Auersperg-Castell <philk@bluedynamics.com>
2021-04-08 09:39:06 +0200

    handle InvalidArgumentError

--- src/cone/firebase/messaging.py
+++ src/cone/firebase/messaging.py
@@ -10,6 +10,7 @@ import firebase_admin
 from firebase_admin import messaging
 from cone.app import ugm_backend
 from cone.firebase.management import get_device_tokens_for_user, unregister_device_token_for_user
+from firebase_admin.exceptions import InvalidArgumentError
 from firebase_admin.messaging import UnregisteredError
 
 logger = logging.getLogger('cone.firebase')
@@ -39,7 +40,7 @@ def send_message_to_user(login: str, data: Dict[str, Any]) -> List[str]:
         try:
             res = send_message(data, token)
             results.append(res)
-        except UnregisteredError as ex:
+        except (UnregisteredError, InvalidArgumentError) as ex:
             unregister_device_token_for_user(login, token)
             logger.exception(f"error sending message to:{token}. the token is not registered in firebase, will be automatically unregistered")
         except Exception as ex:
